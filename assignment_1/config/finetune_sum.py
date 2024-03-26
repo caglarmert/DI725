d1f38101cb1960f0f4739cdb2f37710a7b6c345a
Mert Caglar <mert.caglar@metu.edu.tr>
2024-03-26 23:06:18 +0300

    Add files via upload

---
+++ assignment_1/config/finetune_sum.py
@@ -0,0 +1,23 @@
+import time
+
+out_dir = 'out-sum'
+eval_interval = 5
+eval_iters = 40
+wandb_log = False # feel free to turn on
+wandb_project = 'sum'
+wandb_run_name = 'ft-' + str(time.time())
+
+dataset = 'sum'
+init_from = 'gpt2' # this is the GPT-2 model
+
+# only save checkpoints if the validation loss improves
+always_save_checkpoint = False
+
+# the number of examples per iter:
+batch_size = 1
+gradient_accumulation_steps = 32
+max_iters = 20
+
+# finetune at constant LR
+learning_rate = 3e-5
+decay_lr = False
